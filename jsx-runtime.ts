bd5f6b70d7306302d3c66c65c1166f9b8de8031b
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2021-09-17 21:36:22 +0700

    Replace jsx-runtime proxy ref with observable ref

--- jsx-runtime.ts
+++ jsx-runtime.ts
@@ -20,10 +20,10 @@ const { entries, defineProperties, seal, assign, keys } = Object;
 const { from } = Array;
 
 const Fragment: Record<Key, never> = {};
-const $nodes = Symbol();
+const $cache = Symbol();
 
 const isState = <T>(value: any): value is JSX.State<T> =>
-  value && value[$nodes];
+  value && value[$cache];
 
 const state = <T extends Primitive>(data?: T): JSX.State<NonLiteral<T>> => {
   const nodes = new Set<Node>();
@@ -47,80 +47,26 @@ const state = <T extends Primitive>(data?: T): JSX.State<NonLiteral<T>> => {
   const properties: PropertyDescriptorMap = {
     // Vue ref but handle computed state
     [JSX_STATE_PROPERTY]: { get: observable, set: observable },
-    [$nodes]: { get: () => nodes }, // cache assigned (Attr | Text) for observable()
+    [$cache]: { get: () => nodes }, // cache assigned (Attr | Text) for observable()
   };
   return defineProperties(observable, properties) as any;
 };
 
+export const as = <T extends Element>(trait = Element): JSX.Ref<T> => {
+  const element = () => deElement[$cache] as Element; // just for my auto-completion, should be inlined by the bundler
 
-export const proxy = <T extends Element>(trait = Element): JSX.Ref<T> => {
-  let element: Element | Text = new Text();
-  const safeReplace = (newElement: Element) => {
+  // name is inspired from https://github.com/Rich-Harris/degit
+  function deElement(newElement?: Element | null) {
     if (newElement instanceof trait) {
-      // element.replaceWith(element = newElement); // BUG: see if it cause memory leak
-      element.replaceWith(newElement);
-      element.remove();
-      element = newElement;
-    }
-  };
-  const { proxy, revoke } = Proxy.revocable(trait, {
-    apply(target, self, [ops]) {
-      switch (ops) {
-        case Attr:
-          return from((element as Element).attributes);
-        case Element:
-          return from((element as Element).children);
-        case Node:
-          return from<Node>((element as Element).attributes).concat(
-            ...element.childNodes,
-          );
-        case Text:
-          return from(element.childNodes).filter((node) =>
-            node instanceof Text
-          );
-        case Comment:
-          return from(element.childNodes).filter((node) =>
-            node instanceof Comment
-          );
-        case null:
-          element.remove();
-          return revoke();
-        default:
-          safeReplace(ops);
-          return element;
-      }
-    },
-    construct(target, [newElement]) {
-      if (newElement !== null) {
-        safeReplace(newElement);
-        return proxy;
-      } else {
-        revoke();
-        return element;
-      }
-    },
-    has: (target, key) => (element as Element).hasAttribute(key.toString()),
-    deleteProperty(target, key) {
-      const hasAttr = this.has(target, key);
-      if (hasAttr) (element as Element).removeAttribute(key.toString());
-      return hasAttr;
-    },
-    // TODO: handle layout trashing similiar to fastdom (or better/simpler/smaller)
-    set(target, key, value) {
-      if (key in element) {
-        element[key] = value;
-        return true;
-      } else return false;
-    },
-    get: (target, key) => element[key],
-    // https://stackoverflow.com/a/55124080/5221998
-    ownKeys: () => keys(element),
-    getOwnPropertyDescriptor: (target, key) => ({
-      value: element[key],
-      enumerable: true,
-    }),
-  });
-  return proxy;
+      element().replaceWith(deElement[$cache] = newElement);
+      element().remove();
+    } else if (newElement === null) element().remove();
+    return element();
+  }
+
+  return defineProperties(deElement, {
+    [$cache]: { writable: true },
+  }) as JSX.Ref<T>;
 };
 
 const jsx = (tag, props = {} as Record<Key, any>, ...children) => {
@@ -130,23 +76,21 @@ const jsx = (tag, props = {} as Record<Key, any>, ...children) => {
         mode == SVG_MODE ? "2000/svg" : "1999/xhtml"
       }`;
       const element = document.createElementNS(namespace, tag);
-      const { [JSX_REF_ATTRIBUTE]: proxy$, ...props$ } = props;
-      proxy$(element);
-      // const { [JSX_REF_ATTRIBUTE]: ref, ...props$ } = props;
-      // ref[JSX_REF_PROPERTY] = element;
+      const { [JSX_REF_ATTRIBUTE]: deassign, ...props$ } = props;
+      deassign[$cache] = element;
       for (let [name, value] of entries(props$)) {
         if (name.startsWith("on")) element[name] = value;
         else {
           const attr = assign(document.createAttribute(name), { value });
           if (isState(value)) {
-            value[$nodes].add(assign(attr, { nodeValue: value() }));
+            value[$cache].add(assign(attr, { nodeValue: value() }));
           }
           element.setAttributeNode(attr);
         }
       }
       for (const node of children) {
         if (isState(node)) {
-          node[$nodes].add(
+          node[$cache].add(
             element.appendChild(new Text(node() as string)), // Text has auto .toString()
           );
         } else element.append(node);
@@ -265,10 +209,11 @@ declare global {
       <R extends Element>(
         newElement?: R | null,
       ): typeof newElement extends R ? R : T;
+      [$cache]: Element;
     }
     interface State<T = unknown> {
       (value?: T | ((val: T) => Primitive)): T;
-      readonly [$nodes]: Set<Node>;
+      readonly [$cache]: Set<Node>;
     }
 
     interface Factory extends HTMLFactory, SVGFactory {}
